aa0a04a2c0945e493729305bcde550a3f302b93d
Putra <puterayasa180@gmail.com>
2026-09-21 08:49:48 +0800

    create migrations

---
+++ database/migrations/2026_09_21_002011_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->enum('order_type', ['dine_in', 'takeaway']);
+
+            $table->decimal('total_price', 12, 2);
+
+            $table->enum('status', [
+                'pending',
+                'confirmed',
+                'completed',
+                'cancelled'
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2026_09_21_002051_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('transaction_id')
+                ->constrained('transactions')
+                ->cascadeOnDelete();
+
+            $table->string('midtrans_order_id')->unique();
+
+            $table->string('payment_method')->nullable();
+
+            $table->decimal('amount', 12, 2);
+
+            $table->string('status')->default('pending');
+
+            $table->timestamp('paid_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
